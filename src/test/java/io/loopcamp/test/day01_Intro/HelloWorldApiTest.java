ce1c7cde0b3bf290598b95f86a7f7e278c3c2c14
feyruz-loopcamp <feyruz@loopcamp.io>
2025-03-05 17:37:47 -0500

    First file

---
+++ src/test/java/io/loopcamp/test/day01_Intro/HelloWorldApiTest.java
@@ -0,0 +1,14 @@
+package io.loopcamp.test.day01_Intro;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+public class HelloWorldApiTest {
+
+    
+
+}
